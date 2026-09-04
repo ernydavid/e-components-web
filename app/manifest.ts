3c132f59c51940f59301516f39c245f4335d6b09
Erny Salcedo <ernydavid04@gmail.com>
2026-09-04 18:03:11 -0500

    feat: - manifest for pwa added

---
+++ app/manifest.ts
@@ -0,0 +1,26 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "e.components",
+    short_name: "e.components",
+    description: "Beautifully designed components for your web applications.",
+    start_url: "/",
+    display: "standalone",
+    orientation: "portrait",
+    background_color: "#000000",
+    theme_color: "#000000",
+    icons: [
+      {
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
